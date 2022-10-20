abe8a068973a3f6b45fb1fb57a6890d7e362982f
jwiz <jordanheath945@gmail.com>
2022-10-19 22:55:28 -0400

    finished developing db

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    // Rename .env.EXAMPLE to .env and within that file update the DB_PASSWORD to equal your mysql password
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log('Connected to the silver_db database.')
+);
+
+module.exports = db.promise();
